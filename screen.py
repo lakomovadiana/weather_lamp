a590b10ff10ade674079c36dc53a1150db0854f3
Diana Lakomova <diana.lakomova@gmail.com>
2018-12-08 17:39:11 +0100

    implemented empty matrix display

--- screen.py
+++ screen.py
@@ -1,2 +1,14 @@
+import curses
+stdscr = curses.initscr()
+stdscr.clear()
+
 def draw(weather):
-    print(weather)
+    temperature = weather.get_temperature('celsius')['temp']
+    #stdscr.addstr(0, 0, '{}'.format(temperature))
+    draw_matrix()
+    stdscr.refresh()
+
+def draw_matrix():
+    for i in range(0,24,3):
+        for j in range(0,8):
+            stdscr.addstr(j, i, ' * ')
