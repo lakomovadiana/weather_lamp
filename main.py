9642ac7e28fea2f9f6694c46e85a00cc751d3205
Diana Lakomova <diana.lakomova@gmail.com>
2018-12-13 18:58:48 +0100

    animated rain

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ import screen
 
 w = weather.get()
 temperature = w.get_temperature('celsius')['temp']
-weather_type = "Snow" #w.get_status()
+weather_type = "Rain" #w.get_status()
 frames = logic.get_frames(temperature, weather_type)
+frame_count = len(frames)
+current_frame = 0
 
 def main():    
-    screen.draw(frames[0])
+    screen.draw(frames[current_frame])
 
 while True:
     main()
+    if current_frame >= frame_count - 1:
+        current_frame = 0
+    else:
+        current_frame += 1
+
     time.sleep(1)
