ecd23b0a009c70f85340927b72e5b719d8e2f6c4
Diana Lakomova <diana.lakomova@gmail.com>
2018-12-20 07:47:09 +0100

    animated snow

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import screen
 
 w = weather.get()
 temperature = w.get_temperature('celsius')['temp']
-weather_type = "Rain" #w.get_status()
+weather_type = "Snow" #w.get_status()
 frames = logic.get_frames(temperature, weather_type)
 frame_count = len(frames)
 current_frame = 0
